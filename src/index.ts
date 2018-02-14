8bc2284597be684983521928333289c823edcbdf
Florian Keller <github@floriankeller.de>
2018-02-14 12:01:48 +0100

    Use filter instead of reduce, remove unusable words from wordlist

--- src/index.ts
+++ src/index.ts
@@ -9,8 +9,12 @@ export default class ScrabbleCheater {
 
   public start(): Promise<Array<string>> {
     return this.loadWords()
-      .then(() => {
-        console.info(`${this.words.length} words loaded.`);
+      .then(count => {
+        if (count) {
+          console.info(`${count} word${count > 1 ? 's' : ''} loaded.`);
+        } else {
+          return Promise.reject('No words loaded. Wordlist file corrupt?');
+        }
         return this.readLineAsync();
       })
       .then(letters => this.findMatches(letters));
@@ -19,29 +23,30 @@ export default class ScrabbleCheater {
   private findMatches(letters: string): Array<string> {
     const regex = new RegExp(`^[${letters}]+\$`);
 
-    const matches = this.words
-      .reduce((accumulator: Array<string>, currentValue) => {
-        if (regex.test(currentValue)) accumulator.push(currentValue);
-        return accumulator;
-      }, [])
+    return this.words
+      .filter(value => regex.test(value))
       .sort((a, b) => b.length - a.length);
-
-    return matches;
   }
 
-  private loadWords(): Promise<Array<string>> {
-    return this.readFileAsync(this.wordListPath).then(
-      wordList => (this.words = wordList.split('\n'))
-    );
+  private loadWords(): Promise<number> {
+    const regex = new RegExp('^[A-Za-z]+$', 'g');
+
+    return this.readFileAsync(this.wordListPath).then(wordList => {
+      this.words = wordList
+        .split('\n')
+        .filter(value => regex.test(value));
+
+      return this.words.length;
+    });
   }
 
   private readFileAsync(filePath: string): Promise<string> {
     return new Promise((resolve, reject) => {
       fs.readFile(filePath, (error, data) => {
-        if (error) {
-          reject(error);
-        } else {
+        if (!error) {
           resolve(data.toString());
+        } else {
+          reject(error.message);
         }
       });
     });
@@ -59,7 +64,7 @@ export default class ScrabbleCheater {
         if (letters) {
           resolve(letters);
         } else {
-          reject('No letters entered');
+          reject('No letters entered.');
         }
         rl.close();
       });
